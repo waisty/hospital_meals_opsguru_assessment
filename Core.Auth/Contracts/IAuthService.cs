3ad929c4828b16e5f246dbc7b75237c49be152bb
Wale Ajiboye <waleajiboye@todayssolutions.com.ng>
2026-03-05 14:01:45 -0500

    Created initial backend structure

---
+++ Core.Auth/Contracts/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Auth.Contracts
+{
+    public interface IAuthService
+    {
+    }
+}
---
+++ Core.Auth/InternalModels/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Auth.InternalModels
+{
+    internal class User
+    {
+        public string Username { get; set; } = "";
+        public string PasswordHash { get; set; } = "";
+        public string Name { get; set; } = "";
+        public bool Admin { get; set; }
+        public bool PatientAdmin { get; set; }
+        public bool MealsAdmin { get; set; }
+        public bool MealsUser { get; set; }
+        public bool KitchenUser { get; set; }
+    }
+}
